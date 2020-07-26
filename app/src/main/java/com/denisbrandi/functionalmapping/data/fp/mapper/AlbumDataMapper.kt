ce9b00b349bc0f2277bbacb599255506c5141e32
Denis <dnsbrnd@gmail.com>
2020-07-26 11:38:58 +0100

    Added inline

--- app/src/main/java/com/denisbrandi/functionalmapping/data/fp/mapper/AlbumDataMapper.kt
+++ app/src/main/java/com/denisbrandi/functionalmapping/data/fp/mapper/AlbumDataMapper.kt
@@ -5,7 +5,7 @@ import com.denisbrandi.functionalmapping.data.network.model.NetworkSong
 import com.denisbrandi.functionalmapping.domain.model.Album
 import com.denisbrandi.functionalmapping.domain.model.Song
 
-fun mapAlbumDto(input: NetworkAlbum, mapSongList: (List<NetworkSong>?) -> List<Song>): Album {
+inline fun mapAlbumDto(input: NetworkAlbum, mapSongList: (List<NetworkSong>?) -> List<Song>): Album {
     return Album(
         input.id.orEmpty(),
         input.title.orEmpty(),
